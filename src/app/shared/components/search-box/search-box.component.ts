ae7f45273bf685a6a9b411f78903c04aa151b65f
ChristianTC <christiantambo15@gmail.com>
2023-10-26 13:52:41 -0400

    add Debounce, to get countries list, add new Output() onDebounce

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, OnInit } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,7 +7,9 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer:Subject<string> = new Subject<string>()
 
   @Input()
   public placeholder: string = '';
@@ -14,9 +17,26 @@ export class SearchBoxComponent {
   @Output()
   public onValue:EventEmitter<string> = new EventEmitter()
 
+  @Output()
+  public onDebounce = new EventEmitter<string>()
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value)
+    })
+  }
+
   public emitValue(term:string){
     if (term.length===0) return
     this.onValue.emit(term)
+  }
+
+  onKeyPress(searchTerm: string){
+    this.debouncer.next(searchTerm)
   }
 
 }
