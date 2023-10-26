75882b3b7891a56f7628ff8023ba921eb77c3b17
ChristianTC <christiantambo15@gmail.com>
2023-10-26 11:54:10 -0400

    show info of country in the page

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
-import { count, switchMap, tap } from 'rxjs';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { count, switchMap, tap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,8 +30,7 @@ export class CountryPageComponent implements OnInit {
         if(!country){
           return this.router.navigateByUrl('');
         }
-        console.log('Get Country');
-        return;
+        return this.country = country
       })
   }
 
